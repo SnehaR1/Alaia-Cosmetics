b379d21d522046aece293612d74dd80a8d59bc6b
SnehaR1 <111834509+SnehaR1@users.noreply.github.com>
2023-12-18 00:05:33 +0530

    Update settings.py

--- alaiaecom/settings.py
+++ alaiaecom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-8ve+5kp(+%!tejwfjbul1tl09tieht9zu#p8dkk19-hi!i@2q5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
