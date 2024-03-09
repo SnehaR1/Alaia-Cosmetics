7d8cedb9ee0af72d695a527976be8788a4361a1e
Sneha <sneharavindranathan@gmail.com>
2024-03-09 19:43:42 +0530

    db connection for hosting

--- alaiaecom/settings.py
+++ alaiaecom/settings.py
@@ -75,14 +75,24 @@ WSGI_APPLICATION = "alaiaecom.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "alaiadb",
+#         "USER": "alaia",
+#         "PASSWORD": "Hello@123",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "alaiadb",
-        "USER": "alaia",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "myproject",
+        "USER": "myprojectuser",
         "PASSWORD": "Hello@123",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "",
     }
 }
 # DATABASES = {
